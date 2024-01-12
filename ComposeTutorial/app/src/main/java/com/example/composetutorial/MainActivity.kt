0397e101ed4eb0c780f3343ae8075fcb1e3bb7da
Aleks272 <akylmaah21@student.oulu.fi>
2024-01-12 15:08:11 +0200

    HW1 Publish
    Homework 1 done

---
+++ ComposeTutorial/app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -0,0 +1,152 @@
+package com.example.composetutorial
+
+import android.content.res.Configuration
+import android.os.Bundle
+import android.widget.Space
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.draw.clip
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import com.example.composetutorial.ui.theme.ComposeTutorialTheme
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.offset
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeTutorialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Conversation(messages = SampleData.conversationSample)
+                }
+            }
+        }
+    }
+}
+data class Message(val author: String, val text: String)
+
+@Composable
+fun MyMessage(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.morri),
+            contentDescription = "Profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(32.dp))
+
+        // Is the message expanded or not
+        var isExpanded by remember { mutableStateOf(false) }
+
+        // surfaceColor to be updated when pressed
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary
+            else MaterialTheme.colorScheme.surface,
+        )
+
+        // Toggle variable when this Row is pressed
+        Row(modifier = Modifier
+            .offset(x = 0.dp, y = 10.dp)
+            .clickable { isExpanded = !isExpanded }
+            ) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall,
+                modifier = Modifier
+                    .scale(
+                        scaleX = 2.0F,
+                        scaleY = 2.0F
+                    )
+            )
+            Spacer(modifier = Modifier.width(34.dp))
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 10.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            )
+            {
+                Text(
+                    text = msg.text,
+                    style = MaterialTheme.typography.bodyLarge,
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    modifier = Modifier
+                        .padding(all = 4.dp)
+                        .scale(
+                            scaleX = 1F,
+                            scaleY = 1F
+                        )
+                )
+            }
+        }
+    }
+}
+@Preview(
+    name = "Dark Mode",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+)
+@Composable
+fun MessagePreview() {
+    ComposeTutorialTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            MyMessage(Message("Mörri:", "Pistäppä raksuja tulemmaan"))
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MyMessage(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    ComposeTutorialTheme {
+        Conversation(SampleData.conversationSample)
+    }
+}
---
+++ ComposeTutorial/app/src/main/java/com/example/composetutorial/SampleData.kt
@@ -0,0 +1,79 @@
+package com.example.composetutorial
+
+/**
+ * SampleData from Jetpack Compose Tutorial, modified to include a conversation between cats
+ */
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Mörri",
+            "Noniin. Peukaloiden puutteesta huolimatta opin viimein kirjottamaan"
+        ),
+        Message(
+            "Mörri",
+            """Asiat joihin on tultava muutos:
+Enemmän raksuja kerralla
+Raksuja useammin
+Temppujen teko on loputtava (tämä mikään sirkus ole??)
+Lissää naruja
+Vähemmän harjausta
+Imuri lähimpään ekopisteeseen
+Mikko vois muuttaa pois
+Uus fretti
+Rapsuttaa sais usiammin"""
+        ),
+        Message(
+            "Mörri",
+             "Saapi kuitata sitten kun on asia ymmärretty"
+        ),
+        Message(
+            "Mörri",
+            "Muuten tulee seuraamuksia mitä kukaan meistä ei halua nähä (sinappitykki)"
+        ),
+        Message(
+            "Mörri",
+            "Mikko kiusaa"
+        ),
+        Message(
+            "Mörri",
+            "Ja minä kiusaan takasin"
+        ),
+        Message(
+            "Mörri",
+            "Alkaa olla muuten nälkä"
+        ),
+        Message(
+            "Mörri",
+            "Voisitta vastatakki"
+        ),
+        Message(
+            "Mörri",
+            "Ok meen päiväunille"
+        ),
+        Message(
+            "Mörri",
+            "Heräsin. Nälkä"
+        ),
+        Message(
+            "Mörri",
+            "Haloo"
+        ),
+        Message(
+            "Mörri",
+            "Pissasin muute laatikkoon just (ei se hiekka) :))"
+        ),
+        Message(
+            "Mörri",
+            "Mräy"
+        ),
+        Message(
+            "Mörri",
+            "Söin just ja nyt takas unille"
+        ),
+        Message(
+            "Mörri",
+            "Öitä valmiix"
+        ),
+    )
+}
